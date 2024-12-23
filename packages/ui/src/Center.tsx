8feaab1b6e92b4836f81df3773b95bbe291b48ba
test-in-chinna <chinna.muthangi@gmail.com>
2024-12-23 20:32:41 +0530

    Update Center.tsx

--- packages/ui/src/Center.tsx
+++ packages/ui/src/Center.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 export const Center = ({ children }: { children: React.ReactNode }) => {
+    console.log("lol");
     return <div className="flex justify-center flex-col h-full">
         <div className="flex justify-center">
             {children}
